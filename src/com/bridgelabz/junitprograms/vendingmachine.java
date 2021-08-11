de9e087f23c2c2704bb4228692832bbb4e51ec26
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-11 20:25:17 +0530

    Added Vending Machine program

--- src/com/bridgelabz/junitprograms/vendingmachine.java
+++ src/com/bridgelabz/junitprograms/vendingmachine.java
@@ -8,20 +8,6 @@ public class vendingmachine {
 		System.out.println("Enter change you want from vending machine: ");
 		int amount = sc.nextInt();
 		
-//		//Hashmap initialised
-//		HashMap<Integer, Integer> change = new HashMap<Integer, Integer>();
-//		change.put(1000,0);
-//		change.put(500,0);
-//		change.put(100,0);
-//		change.put(50,0);
-//		change.put(10,0);
-//		change.put(5,0);
-//		change.put(2,0);
-//		change.put(1,0);
-//		
-//		while(amount != 0) {
-//			
-//		}
 		
 		int[] arr = new int[]{1000,500,100,50,10,5,2,1};
 		int[] change = new int[]{0,0,0,0,0,0,0,0};
